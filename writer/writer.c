5b6a816e21854f67abdd166be492a2201b32c9b7
Petr Novak <pnovak@koukaam.se>
2012-02-03 13:57:31 +0100

    writer that does not segfault

--- writer/writer.c
+++ writer/writer.c
@@ -3,21 +3,75 @@
 
 #undef printf
 
+static AVStream *add_audio_stream(AVFormatContext *oc, enum CodecID codec_id)
+{
+    AVCodecContext *c;
+    AVStream *st;
+
+    st = avformat_new_stream(oc, NULL);
+    if (!st) {
+        printf("Could not alloc stream\n");
+        return NULL;
+    }
+    st->id = 1;
+
+    c = st->codec;
+    c->codec_id = codec_id;
+    c->codec_type = AVMEDIA_TYPE_AUDIO;
+
+    /* put sample parameters */
+    c->sample_fmt = AV_SAMPLE_FMT_S16;
+    c->bit_rate = 64000;
+    c->sample_rate = 44100;
+    c->channels = 2;
+
+    // some formats want stream headers to be separate
+    if (oc->oformat->flags & AVFMT_GLOBALHEADER)
+        c->flags |= CODEC_FLAG_GLOBAL_HEADER;
+
+    return st;
+}
+
 int main()
 {
 	extern AVOutputFormat ff_matroska_audio_muxer;
 	av_register_output_format(&ff_matroska_audio_muxer);
-	AVFormatContext * oc = avformat_alloc_context();
+	extern URLProtocol ff_file_protocol;
+	ffurl_register_protocol(&ff_file_protocol, sizeof(ff_file_protocol));
+	
 	AVOutputFormat * fmt = av_guess_format("matroska", "mka", "audio/x-matroska");
 	if (fmt == NULL)
 	{
 		printf("FAIL!\n");
 		return 1;
 	}
-	oc->oformat = fmt;
+	
+	AVFormatContext * oc;
+	const char * filename = "/tmp/output.mka";
+	int ret = avformat_alloc_output_context2(&oc, fmt, NULL, filename);
+	if (ret < 0)
+	{
+		printf("FAIL!\n");
+		return 1;
+	}
+	
+	AVStream * avs = add_audio_stream(oc, CODEC_ID_AAC);
+	if (avs == NULL)
+	{
+		printf("FAIL!\n");
+		return 1;
+	}
+	
+	av_dump_format(oc, 0, filename, 1);
+	
+	ret = avio_open(&oc->pb, filename, AVIO_FLAG_WRITE);
+	if (ret < 0) {
+            printf("FAIL! Could not open '%s'\n", filename);
+            return 1;
+        }
 	
 	AVDictionary * options = NULL;
-	int ret = avformat_write_header(oc, &options);
+	ret = avformat_write_header(oc, &options);
 	if (ret != 0)
 	{
 		AVDictionaryEntry * cur;
@@ -35,7 +89,7 @@ int main()
 		AVPacket packet;
 		av_init_packet(&packet);
 		packet.size = sizeof(foo);
-		packet.stream_index= 0;
+		packet.stream_index = 0;
 		packet.data = foo;
 		packet.flags |= AV_PKT_FLAG_KEY;
 		
